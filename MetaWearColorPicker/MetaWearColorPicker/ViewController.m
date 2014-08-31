9afbb4cb2ee8ece215a3fdadd9f75e26793f6eed
Jonathan King <jonathanlking@me.com>
2014-08-31 17:47:36 +0100

    Built basic structure in IB

--- MetaWearColorPicker/MetaWearColorPicker/ViewController.m
+++ MetaWearColorPicker/MetaWearColorPicker/ViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <MetaWear/MetaWear.h>
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UILabel *connectionStatusLabel;
+@property (strong, nonatomic) IBOutlet UILabel *currentColorLabel;
+@property (strong, nonatomic) IBOutlet UIView *colorView;
+- (IBAction)sliderValueChanged:(UISlider *)sender;
+@property (strong, nonatomic) MBLMetaWear *device;
+- (void)connectToDevice;
+@property (strong, nonatomic) UIColor *color;
 @end
 
 @implementation ViewController
@@ -18,6 +25,42 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self connectToDevice];
+}
+
+- (void)connectToDevice
+{
+    self.connectionStatusLabel.text = @"Searching for devices";
+    
+    // Begin scanning for MetaWare boards
+    [[MBLMetaWearManager sharedManager] startScanForMetaWearsWithHandler:^(NSArray *array)
+    {
+        // Hooray! We found a MetaWear board, so stop scanning for more
+//        [[MBLMetaWearManager sharedManager] stopScanForMetaWears];
+        // Connect to the board we found
+        MBLMetaWear *device = [array firstObject];
+        [[MBLMetaWearManager sharedManager] connectMetaWear:device withHandler:^(NSError *error)
+        {
+            if (!error)
+            {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                
+                    self.device = device;
+                    self.connectionStatusLabel.text = @"Connected";
+                });
+            }
+        }];
+    }];
+}
+
+- (void)setDevice:(MBLMetaWear *)device {
+    
+    if (device) [self enableSliders];
+}
+
+- (void)enableSliders {
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +69,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+}
 @end
